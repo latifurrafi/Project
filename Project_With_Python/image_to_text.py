1444acef0a92e22b13fe3bcc4292813f0fe3c942
Rafi <latifurrafi@gmail.com>
2024-11-10 15:19:21 +0600

    Script

---
+++ Project_With_Python/image_to_text.py
@@ -0,0 +1,10 @@
+import pytesseract
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+
+image = Image.open('/home/rafi/VSCODE/CodeCraze/Project_With_Python/test.png')
+
+word = pytesseract.image_to_string(image)
+
+print("Extracted Text:\n", word)
