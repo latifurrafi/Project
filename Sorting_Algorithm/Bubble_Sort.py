f87b09e40151f353fe829b46868d54a91501b782
Rafi <latifurrafi@gmail.com>
2024-11-17 01:15:21 +0600

    SortingAlgorithm

---
+++ Sorting_Algorithm/Bubble_Sort.py
@@ -0,0 +1,17 @@
+def bubbleSort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if (swapped == False):
+            break
+
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    print("Before_Sorting : ",arr)
+    bubbleSort(arr)
+    print("After_Sorting : ",arr)
